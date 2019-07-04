918de9c0730485222c5b714087014d6108cdb44e
Connection Refused <ConnectionRefusedX@protonmail.com>
2019-07-05 00:00:02 +0800

    Upload current code

---
+++ SimpleDNS/Properties/AssemblyInfo.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Reflection;
+using System.Runtime.InteropServices;
+
+// 有关程序集的一般信息由以下
+// 控制。更改这些特性值可修改
+// 与程序集关联的信息。
+[assembly: AssemblyTitle("SimpleDNS")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("SimpleDNS")]
+[assembly: AssemblyCopyright("Copyright ©  2019")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// 将 ComVisible 设置为 false 会使此程序集中的类型
+//对 COM 组件不可见。如果需要从 COM 访问此程序集中的类型
+//请将此类型的 ComVisible 特性设置为 true。
+[assembly: ComVisible(false)]
+
+[assembly: CLSCompliant(true)]
+
+// 如果此项目向 COM 公开，则下列 GUID 用于类型库的 ID
+[assembly: Guid("1fff2022-3d9a-4836-893e-1851918dd3b7")]
+
+// 程序集的版本信息由下列四个值组成: 
+//
+//      主版本
+//      次版本
+//      生成号
+//      修订号
+//
+//可以指定所有这些值，也可以使用“生成号”和“修订号”的默认值
+//通过使用 "*"，如下所示:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ SimpleDNS/SimpleDNS.cs
@@ -0,0 +1,15 @@
+﻿using System.Net;
+
+namespace SimpleDNS
+{
+    public class SimpleDNS
+    {
+        public static DNS.Server.DnsServer Server;
+
+        public static void Main(string[] args)
+        {
+            Server = new DNS.Server.DnsServer(new Resolver());
+            Server.Listen(new IPEndPoint(IPAddress.IPv6Any, 53)).Wait();
+        }
+    }
+}
